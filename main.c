d5b70562861862b4488bb749d4a8a06620cc9a35
kraken <narminelkilani@gmail.Com>
2023-12-20 09:49:46 +0100

    minor rect, add comments

--- main.c
+++ main.c
@@ -1,22 +1,27 @@
+//#<include <MLV/MLV_all.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <MLV/MLV_all.h>
+#include<stdlib.h>
+#include<time.h>
 
+
+// Dimensions de la grille en nombre de cases (origine en haut a gauche) :
 #define COLONNES 12
 #define LIGNES 18
-
+// Les deux camps :
 #define ABEILLE 'A'
-#define FRELON 'F'
-#define REINE 'r'
+#define FRELONS 'F'
+// Les types d'unites :
+#define REINEA 'r'
+#define REINEF 'rf'
 #define OUVRIERE 'o'
 #define ESCADRON 'e'
 #define GUERRIERE 'g'
 #define FREL 'f'
 #define RUCHE 'R'
 #define NID 'N'
+// Pour la recolte de pollen
 #define RECOLTE 'p'
-
+// Les temps necessaires a la production :
 #define TREINEA 8
 #define TREINEF 8
 #define TOUVRIERE 2
@@ -25,6 +30,7 @@
 #define TFRELON 5
 #define TRECOLTE 4
 
+// Les couts necessaires a la production :
 #define CREINEA 7
 #define CREINEF 8
 #define COUVRIERE 3
@@ -33,37 +39,40 @@
 #define CFRELON 3
 #define CRUCHE 10
 #define CNID 10
-
+// La force des unites
 #define FREINE 6
 #define FOUVRIERE 1
 #define FGUERRIERE 5
 #define FESCADRON 12
 #define FFRELON 8
 
-typedef struct {
-    char camp;
-    char type;
-    int force;
-    int posx, posy;
-    int destx, desty;
-    char production;
-    int temps;
-    int toursrestant;
-    struct unite* usuiv, * uprec;
-    struct unite* colsuiv, * colprec;
-    struct unite* vsuiv, * vprec;
-} Unite, * UListe;
+// La structure Unite :
+typedef struct unite {
+char camp; // ABEILLE ou FRELON
+char type; // RUCHE, NID, REINE, OUVRIERE, GUERRIERE, ESCADRON ou FRELON
+int force; // la force de l'unite
+int posx, posy; // position actuelle sur la grille
+int destx, desty; // destination (negatif si immobile)
+char production; // production d'une ruche ou d'un nid et RECOLTE pour la recolte de pollen
+int temps; // nombres de tours total pour cette production
+int toursrestant; // tours restant pour cette production
+struct unite *usuiv, *uprec; // liste des unites affiliees a une ruche ou un nid
+struct unite *colsuiv, *colprec; // liste des autres ruches ou nids (colonies) du même camp
+struct unite *vsuiv, *vprec; // liste des autres unites sur la meme case
+} Unite, *UListe;
 
+// La structure Case :
 typedef struct {
-    Unite* colonie;
-    UListe occupant;
+Unite* colonie; // S'il y a une ruche ou un nid sur la case
+Unite* occupant; // les autres occupants de la case
 } Case;
 
+// La structure Grille :
 typedef struct {
-    Case plateau[LIGNES][COLONNES];
-    UListe abeille, frelon;
-    int tour;
-    int ressourcesAbeille, ressourcesFrelon;
+//Case plateau[X][Y];
+Unite* abeille, frelon;
+int tour; // Numero du tour
+int ressourcesAbeille, ressourcesFrelon;
 } Grille;
 
 void initializeGrid(Unite grid[LIGNES][COLONNES]) {
@@ -73,14 +82,15 @@ void initializeGrid(Unite grid[LIGNES][COLONNES]) {
         }
     }
 
-    grid[0][0] = (Unite){RUCHE, REINE, FREINE, 0, 0};
-    grid[0][1] = (Unite){RUCHE, OUVRIERE, FOUVRIERE, 0, 1};
-    grid[0][2] = (Unite){RUCHE, GUERRIERE, FGUERRIERE, 0, 2};
-    grid[0][3] = (Unite){RUCHE, ESCADRON, FESCADRON, 0, 3};
+    grid[0][0] = (Unite){ABEILLE, RUCHE, .posx = 0, .posy = 0};
+               //(Unite){.camp = ABEILLE, .type = RUCHE, .posx = 0, .posy = 0}
+    grid[0][1] = (Unite){ABEILLE, OUVRIERE, FOUVRIERE, 0, 1};
+    grid[0][2] = (Unite){ABEILLE, GUERRIERE, FGUERRIERE, 0, 2};
+    grid[0][3] = (Unite){ABEILLE, ESCADRON, FESCADRON, 0, 3};
 
-    grid[LIGNES - 1][COLONNES - 1] = (Unite){NID, REINE, FREINE, LIGNES - 1, COLONNES - 1};
-    grid[LIGNES - 1][COLONNES - 2] = (Unite){NID, FREL, FFRELON, LIGNES - 1, COLONNES - 2};
-    grid[LIGNES - 1][COLONNES - 3] = (Unite){NID, FREL, FFRELON, LIGNES - 1, COLONNES - 3};
+    grid[LIGNES - 1][COLONNES - 1] = (Unite){FRELON, REINE, FREINE, LIGNES - 1, COLONNES - 1};//grid[17][11]
+    grid[LIGNES - 1][COLONNES - 2] = (Unite){FFRELON, FREL, FFRELON, LIGNES - 1, COLONNES - 2}; //gird[17][10]
+    grid[LIGNES - 1][COLONNES - 3] = (Unite){FRELON, FREL, FFRELON, LIGNES - 1, COLONNES - 3}; //grid[17][9]
 }
 
 void printGrid(Unite grid[LIGNES][COLONNES]) {
@@ -100,7 +110,7 @@ void printGrid(Unite grid[LIGNES][COLONNES]) {
                     printf(" R ");
                     break;
                 case REINE:
-                    printf(" A ");
+                    printf(" r ");
                     break;
                 case GUERRIERE:
                     printf(" G ");
